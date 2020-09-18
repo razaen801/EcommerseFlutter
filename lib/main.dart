24827311422f725a9030f1a34f4ed582ca4fff8e
Kushal Shrestha <kushalshrestha85@gmail.com>
2020-09-18 16:50:48 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:rohi_furniture_app/screen/career_screen.dart';
-import 'package:rohi_furniture_app/screen/product_detail_screen.dart';
-import 'package:rohi_furniture_app/widgets/user_detail_view.dart';
-=======
->>>>>>> 647d8e2e8536b03c37b61c17b501cf15408736cb
 import 'package:provider/provider.dart';
 import 'package:rohi_furniture_app/provider/product_provider.dart';
 import 'package:rohi_furniture_app/provider/user_provider.dart';
 import 'package:rohi_furniture_app/screen/home_page_screen.dart';
 import 'package:rohi_furniture_app/screen/product_list_category_screen.dart';
+
 
 void main() {
   runApp(MyApp());
