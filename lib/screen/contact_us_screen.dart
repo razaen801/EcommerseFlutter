d62d836a08b5b1986d8db7b86486422ca2644d17
Ashmita Shrestha <asisthre@gmail.com>
2020-09-22 10:47:11 +0000

    contact us screen added

--- lib/screen/contact_us_screen.dart
+++ lib/screen/contact_us_screen.dart
@@ -1,8 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:rohi_furniture_app/widgets/infoCard.dart';
+import 'package:rohi_furniture_app/widgets/slider.dart';
+import 'package:url_launcher/url_launcher.dart' as Luncher;
 
-class ContactUsScreen extends StatelessWidget {
+const address= 'Our Address\n'
+    'Kuleshwor,near Ganeshthan mandir';
+const openingHours='Opening Hours\n'
+    '09:00 AM to 06:00 PM';
+const phone= '01 4271760';
+const website='Website\n'
+    'http://rohiint.com/';
+const email='Email\n'
+    'info@rohitint.com';
+
+class ContactUsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Contact(),
+    );
   }
+
+}
+
+class Contact extends StatelessWidget{
+  void openMap() async {
+    String googleUrl =
+        'https://www.google.com/maps/search/?api=1&query=	27.7167,	85.3167';
+    if (await Luncher.canLaunch(googleUrl)) {
+      await Luncher.launch(googleUrl);
+    } else {
+      throw 'Could not open the map.';
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:
+      SafeArea(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                color:  Colors.black,
+                child: CarouselSliderDemo(),
+              ),
+              // CircleAvatar(
+              //   radius: 100,
+              //   backgroundImage: AssetImage('images/logo_rohi.jpg'),
+              // ),
+              // Text('Rohi Internatinal',
+              // style: TextStyle(
+              //     fontSize:40.0,
+              //     color:Colors.white,
+              //     fontWeight: FontWeight.bold,
+              // ),),
+              SizedBox(
+                height: 20,
+                width: 300,
+                child: Divider(
+                  color:Colors.red,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Column(
+                  children: [
+                    InfoCard(text: phone,
+                      icon: Icons.phone,
+                      onPressed: () async{
+                        String removeSpacePhone= phone.replaceAll(new RegExp(r"\s+\b|\b\s"), "");
+                        final phoneNumber= 'tel:$removeSpacePhone';
+                        if (await Luncher.canLaunch(phoneNumber)) {
+                          await Luncher.launch(phoneNumber);
+                        } else {
+                          print(phoneNumber);
+                        }
+                      },
+                    ),
+                    InfoCard(
+                      text: address, icon: Icons.location_on,
+                      onPressed: (){
+                        openMap();
+                        print('Address');
+                      },
+                    ),
+                    InfoCard(
+                      text: openingHours,
+                      icon: Icons.schedule,
+                      onPressed: (){
+                        print('Opening Hours');
+                      },
+                    ),
+                    InfoCard(
+                      text: website,
+                      icon: Icons.web,
+                      onPressed: () async{
+                        if (await Luncher.canLaunch(website)) {
+                          await Luncher.launch(website);
+                        } else {
+                          throw 'Could not launch $website';
+                        }
+                      },
+                    ),
+                    InfoCard(
+                      text: email,
+                      icon: Icons.mail,
+                      onPressed: () async{
+                        final emailAddress= 'mailto:$email';
+                        if (await Luncher.canLaunch(emailAddress)) {
+                          await Luncher.launch(emailAddress);
+                        } else {
+                          throw 'Could not launch $emailAddress';
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
 }
---
+++ lib/widgets/slider.dart
@@ -0,0 +1,93 @@
+
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class CarouselSliderDemo extends StatefulWidget{
+  CarouselSliderDemo(): super();
+  final String title= "Carousel Slider";
+  @override
+  CarouselSliderDemoState createState() => CarouselSliderDemoState();
+
+}
+
+class CarouselSliderDemoState extends State<CarouselSliderDemo> {
+  int _current=0;
+  final List<String> imgList=[
+    "assets/art_grass.jpg",
+    "assets/carpets.jpg",
+    "assets/Laminate.jpg",
+    "assets/chairs.jpg",
+    "assets/Flooring.jpg",
+          ];
+
+
+
+  // List<T> map<T>(List list, Function handler){
+  //   List<T> result = [];
+  //   for(var i=0;1<list.length;i++){
+  //     result.add(handler(i, list[i]));
+  //   }
+  //   return result;
+  // }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CarouselSlider(
+                options: CarouselOptions(initialPage: 0,
+                enlargeCenterPage: true,
+                autoPlay: true,
+                reverse: false,
+                enableInfiniteScroll: true,
+                autoPlayInterval: Duration(seconds: 2),
+                autoPlayAnimationDuration: Duration(milliseconds: 2000),
+                pauseAutoPlayOnTouch:true,
+                scrollDirection: Axis.horizontal),
+              //   carouselController:(index) {
+              //     setState(() {
+              //       _current=index;
+              //     });
+              // },
+              items: imgList.map((imgList){
+                return Builder(
+                  builder: (BuildContext context){
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        color: Colors.teal,
+                      ),
+                      child:Image.asset(imgList, fit: BoxFit.fill),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+
+            // SizedBox(
+            //   height: 20,
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: map<Widget>(imgList, (index){
+            //     return Container(
+            //       width: 10,
+            //       height: 10,
+            //       margin: EdgeInsets.symmetric(vertical:10, horizontal: 2.0 ),
+            //       decoration: BoxDecoration(shape: BoxShape.circle,
+            //       color: _current== index? Colors.redAccent: Colors.green),
+            //     );
+            //   }),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
