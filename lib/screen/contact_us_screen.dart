e0a7872c9d38a24228c2a291646e70164034c19a
Kushal Shrestha <kushalshrestha85@gmail.com>
2020-09-23 14:06:36 +0000

    Update contact_us_screen.dart

--- lib/screen/contact_us_screen.dart
+++ lib/screen/contact_us_screen.dart
@@ -3,17 +3,15 @@ import 'package:rohi_furniture_app/widgets/infoCard.dart';
 import 'package:rohi_furniture_app/widgets/slider.dart';
 import 'package:url_launcher/url_launcher.dart' as Luncher;
 
-const address= 'Our Address\n'
+const address = 'Our Address\n'
     'Kuleshwor,near Ganeshthan mandir';
-const openingHours='Opening Hours\n'
+const openingHours = 'Opening Hours\n'
     '09:00 AM to 06:00 PM';
-const phone= '01 4271760';
-const website='Website\n'
-    'http://rohiint.com/';
-const email='Email\n'
-    'info@rohitint.com';
+const phone = '01 4271760';
+const website = 'http://rohiint.com/';
+const email = 'info@rohitint.com';
 
-class ContactUsScreen extends StatelessWidget{
+class ContactUsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,25 +19,24 @@ class ContactUsScreen extends StatelessWidget{
       home: Contact(),
     );
   }
-
 }
 
-class Contact extends StatelessWidget{
+class Contact extends StatelessWidget {
   void openMap() async {
     String googleUrl =
-        'https://www.google.com/maps/search/?api=1&query=	27.7167,	85.3167';
+        'https://www.google.com/maps/search/?api=1&query=	27.6889697,	85.2979805';
     if (await Luncher.canLaunch(googleUrl)) {
       await Luncher.launch(googleUrl);
     } else {
       throw 'Could not open the map.';
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body:
-      SafeArea(
+      body: SafeArea(
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: Column(
@@ -48,7 +45,7 @@ class Contact extends StatelessWidget{
               Container(
                 height: 250,
                 width: MediaQuery.of(context).size.width,
-                color:  Colors.black,
+                color: Colors.black,
                 child: CarouselSliderDemo(),
               ),
               // CircleAvatar(
@@ -63,20 +60,22 @@ class Contact extends StatelessWidget{
               // ),),
               SizedBox(
                 height: 20,
-                width: 300,
+                width: MediaQuery.of(context).size.width,
                 child: Divider(
-                  color:Colors.red,
+                  color: Colors.red,
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(6.0),
                 child: Column(
                   children: [
-                    InfoCard(text: phone,
+                    InfoCard(
+                      text: "Phone\n$phone",
                       icon: Icons.phone,
-                      onPressed: () async{
-                        String removeSpacePhone= phone.replaceAll(new RegExp(r"\s+\b|\b\s"), "");
-                        final phoneNumber= 'tel:$removeSpacePhone';
+                      onPressed: () async {
+                        String removeSpacePhone =
+                            phone.replaceAll(new RegExp(r"\s+\b|\b\s"), "");
+                        final phoneNumber = 'tel:$removeSpacePhone';
                         if (await Luncher.canLaunch(phoneNumber)) {
                           await Luncher.launch(phoneNumber);
                         } else {
@@ -85,23 +84,23 @@ class Contact extends StatelessWidget{
                       },
                     ),
                     InfoCard(
-                      text: address, icon: Icons.location_on,
-                      onPressed: (){
+                      text: address,
+                      icon: Icons.location_on,
+                      onPressed: () {
                         openMap();
-                        print('Address');
                       },
                     ),
                     InfoCard(
                       text: openingHours,
                       icon: Icons.schedule,
-                      onPressed: (){
+                      onPressed: () {
                         print('Opening Hours');
                       },
                     ),
                     InfoCard(
-                      text: website,
+                      text: "Website\n$website",
                       icon: Icons.web,
-                      onPressed: () async{
+                      onPressed: () async {
                         if (await Luncher.canLaunch(website)) {
                           await Luncher.launch(website);
                         } else {
@@ -110,10 +109,10 @@ class Contact extends StatelessWidget{
                       },
                     ),
                     InfoCard(
-                      text: email,
+                      text: "Email\n$email",
                       icon: Icons.mail,
-                      onPressed: () async{
-                        final emailAddress= 'mailto:$email';
+                      onPressed: () async {
+                        final emailAddress = 'mailto:$email';
                         if (await Luncher.canLaunch(emailAddress)) {
                           await Luncher.launch(emailAddress);
                         } else {
@@ -124,12 +123,10 @@ class Contact extends StatelessWidget{
                   ],
                 ),
               ),
-
             ],
           ),
         ),
       ),
     );
   }
-
 }
